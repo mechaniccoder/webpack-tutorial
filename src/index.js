7ab60a8072a310a2c9e45b509a1e097c334eecf3
Seung-Hwan-Yu <yuseunghwan94@gmail.com>
2021-02-21 15:25:56 +0900

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from "lodash";
+import "./style.css";
+import Icon from "./tdd.png";
 
 function component() {
   const div = document.createElement("div");
 
   div.innerHTML = _.join(["Hello", "World"], " ");
+  div.classList.add("hello");
+
+  const tddIcon = new Image();
+  tddIcon.src = Icon;
+  div.appendChild(tddIcon);
 
   return div;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Asset Management",
+    }),
+  ],
 };
